9412fd188d79cbcfb9bec64282279f36680a3ec6
mustafasoft <mustafat@softinnovas.com>
2021-12-16 23:25:07 +0300

    js-104

--- JS-103/exercise.js
+++ JS-103/exercise.js
@@ -12,20 +12,22 @@
 !(4 === '4')
 
 
-Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:You are old enough to drive but if not 18 give feedback to wait for the years he supposed to wait for. Output:
+//Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:You are old enough to drive but if not 18 give feedback to wait for the years he supposed to wait for. Output:
+//Enter your age: 30
+//You are old enough to drive.
+//Enter your age:15
+//You are left with 3 years to drive.
 
-Enter your age: 30
-You are old enough to drive.
-Output:
 
-Enter your age:15
-You are left with 3 years to drive.
-Compare the values of myAge and yourAge using if … else. Based on the comparison log to console who is older (me or you). Use prompt(“Enter your age:”) to get the age as input. Output:
 
-Enter your age: 30
-You are 5 years older than me.
-If a is greater than b return a is greater than b else a is less than b. Output:
+//Compare the values of myAge and yourAge using if … else. Based on the comparison log to console who is older (me or you). Use prompt(“Enter your age:”) to get the age as input. Output:
+//Enter your age: 30
+//You are 5 years older than me.
 
- let a = 4
- let b = 3
- 4 is greater than 3
+
+
+
+//If a is greater than b return a is greater than b else a is less than b. Output:
+// let a = 4
+// let b = 3
+// 4 is greater than 3
---
+++ JS-104/homework.js
@@ -0,0 +1,25 @@
+//Write a function findMax that takes three arguments and returns their maximum.
+
+
+
+
+
+//Declare a function name evensAndOdds. It takes a positive integer (from prompt) as parameter and prints out the number even or odd.
+
+
+
+
+
+//Write a function which generates a randomUserIp. Ex: 168.127.25.2
+
+
+
+
+
+
+//Declare a function fullName and now it takes firstName, lastName as a parameters and it returns your full - name with the count of characters inside your name. "Mustafa TT - 10 karakter"
+//Rewrite your function to print a result according to your name ending : Ex. "Mustafa'dan sevgilerle" , "Ali'den sevgilerle", "Fatoş'tan sevgilerle".
+//Challenge: take names from prompt.
+
+
+
